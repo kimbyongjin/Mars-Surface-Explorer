139609f014d9086ba40de6e40d6a3eee7cf8e82f
Daniel Kim <nautilus@Nautilus.local>
2019-06-21 14:55:39 -0700

    Delete unecessary react components, configure eslint, mongo db and refactor server to es6

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const apiKey = require('../authentication');
+const path = require('path');
+const cors = require('cors');
+const items = require('../database-mongo');
 
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
-// var items = require('../database-mongo');
 
 const app = express();
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use(bodyParser());
+app.use(cors());
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
+app.get('/items', (req, res) => {
+  items.selectAll((err, data) => {
+    if (err) {
       res.sendStatus(500);
     } else {
       res.json(data);
@@ -20,6 +23,4 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
-});
+app.listen(3000, () => console.log('listening on port 3000!'));
