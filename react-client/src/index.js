cdf813a8ea0cbf59959c43995a9f62bb8a2c7a44
Daniel Kim <nautilus@Nautilus.local>
2019-06-21 12:08:57 -0700

    Add restructuring file system and extenstions for consistent filename ocnvention

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import List from './components/List.js';
 
 class App extends React.Component {
   constructor(props) {
