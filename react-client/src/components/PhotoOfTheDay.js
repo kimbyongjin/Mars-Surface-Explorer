28fa6bf32426dd98391542e8630851ad5e01c980
Daniel Kim <nautilus@nautilus.attlocal.net>
2019-08-29 12:52:03 -0700

    Reposition POTD description below photo viewer, resize desc, and place text into standalone <p> element

--- react-client/src/components/PhotoOfTheDay.js
+++ react-client/src/components/PhotoOfTheDay.js
@@ -3,9 +3,11 @@ import React from 'react';
 const PhotoOfTheDay = (props) => {
   const { potd } = props;
   return (
-    <div className="potd-container">NASA Photo of the Day
-      {potd.explanation}
+    <div className="potd-container">
       <img src={potd.url} alt="photo-of-the-day" className="potd"></img>
+      <p className="potd-container">
+        {potd.explanation}
+      </p>
     </div>
     );
 };
