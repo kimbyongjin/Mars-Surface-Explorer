201350543e71530eb39ea4340b7c4850f3a189dd
Daniel Kim <nautilus@Nautilus.local>
2019-10-25 17:38:56 -0700

    Added authentication instructions and notes about protecting online credentials

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ Select a rover, camera, and Martian date then click the "Explore Mars" button.
 
 ## Instructions to run locally:
 
+- IMPORTANT: To run this application, you must enable API authentication. To do so, closely follow the instructions in the file ```EDIT-THIS-FILE-authentication.js```
+
+- You will have to change the file name to ```authentication.js```. Applying for your own unique API key is optional and free. But, feel free to test out the application with the supplied ```'DEMO_KEY'```. Just take note of the demo rate limits.
+
+- ALWAYS ENSURE THAT YOU DO NOT COMPROMISE YOUR ONLINE CREDENTIALS. IF YOU DO NOT KNOW HOW TO PROPERLY IGNORE FILES, LOOK IT UP.
+
 - You will need an installation of Node.js on your machine. You can check out [this cool blog](https://www.taniarascia.com/how-to-install-and-use-node-js-and-npm-mac-and-windows/) or the [official Node.js site](https://nodejs.org/en/) to install the latest LTS (Long Term Support) build.
 
 - After installing node, make sure to restart terminal.
---
+++ react-client/src/EDIT-THIS-FILE-authentication.js
@@ -0,0 +1,36 @@
+module.exports = 'DEMO_KEY';
+
+/*
+In order to use this application, regardless of your authentication credential, you MUST enable API
+  authentication.
+
+You MUST change the name of this file from:
+
+'EDIT-THIS-FILE-authentication.js' to 'authentication.js'
+
+The above 'DEMO_KEY' is a valid, free authentication credential to access the NASA.data API.
+  However, there are time-sensitive query limits restricting how often you may access
+  the API. If you would like to apply for your own, unique NASA.data API credential,
+  it is still free. And, the query limits are much less restrictive.
+
+You can apply for your own API key at: https://api.nasa.gov/index.html
+
+To enable your own authentication key, replace 'DEMO_KEY' in the export statement above
+  with your own key. Make sure that you're exporting a string value.
+
+DEMO_KEY rate limits:
+- Hourly Limit: 30 requests per IP address per hour
+- Daily Limit: 50 requests per IP address per day
+
+Web service rate liimits:
+- Hourly Limit: 1,000 requests per hour
+
+NOTE: The file name 'authentication.js' is already included in this repo's .gitignore file.
+  So, simply changing the file name BEFORE inserting your API key will prevent unintentional
+  credential vulnerabilities.
+
+  But, make sure that you know what you're doing. And, if you choose to apply for your
+  own API authentication key, double-check that you are properly ignoring the file.
+
+  Do not publish your authentication credentials to GitHub or anywhere else on the internet.
+*/
