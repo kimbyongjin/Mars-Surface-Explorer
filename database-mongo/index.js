5b615017d5280e70cde43599ca5e23676b07c1a8
Daniel Kim <nautilus@50-1-86-126.dsl.dynamic.fusionbroadband.com>
2019-10-27 16:33:50 -0700

    Remove all database functionality until is is built as a feature

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://localhost/test');
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error', () => {
-  console.log('mongoose connection error');
-});
+// db.on('error', () => {
+//   console.log('mongoose connection error');
+// });
 
-db.once('open', () => {
-  console.log('mongoose connected successfully');
-});
+// db.once('open', () => {
+//   console.log('mongoose connected successfully');
+// });
 
-const itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String,
-});
+// const itemSchema = mongoose.Schema({
+//   quantity: Number,
+//   description: String,
+// });
 
-const Item = mongoose.model('Item', itemSchema);
+// const Item = mongoose.model('Item', itemSchema);
 
-const selectAll = (callback) => {
-  Item.find({}, (err, items) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
-  });
-};
+// const selectAll = (callback) => {
+//   Item.find({}, (err, items) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, items);
+//     }
+//   });
+// };
 
-// TODO: build out feature allowing users to save noteworthy photo queries
+// // TODO: build out feature allowing users to save noteworthy photo queries
 
-module.exports.selectAll = selectAll;
+// module.exports.selectAll = selectAll;
